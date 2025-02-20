3006fcdb8ac21dfce2005ce348d999a9985f0f03
Jean-Philippe Arias Sanchez <jpgosan@gmail.com>
2025-02-20 15:59:46 -0500

    Define App UI
    Install Tailwind
    Add Tailwind to CSS
    Add Google Fonts (Symbols)
    Create UI Skeleton for App
    Install Stylelint and Configs
    Remove CLI generated files and code
    Add Sass
    Add logo
    Replace Favicon

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,80 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  <div class="logo"><img src="./assets/logo-white.png" alt="Morgan Stanley Logo"></div>
   <main>
-    <TheWelcome />
+    <div class="upload-container">
+      <div class="upload-box rounded-xl aspect-(--my-aspect-ratio) bg-white py-8 px-20 text-center">
+        <div class="text-lg font-medium">Please Upload JSON File to Analyze</div>
+        <div class="text-lg self-center font-medium">
+          <div>
+            <span class="material-symbols-outlined upload-icon ">upload</span>
+          </div>
+          Or drag and drop it
+        </div>
+        <div class="text-lg text-center">
+          <button type="button" class="
+          text-white
+          uppercase
+          bg-gray-800
+          hover:bg-gray-900
+          focus:outline-none
+          focus:ring-4
+          focus:ring-blue-300
+          font-medium
+          rounded-lg
+          text-base
+          px-5
+          py-2.5
+          cursor-pointer">Upload File</button>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style lang="scss">
+#app {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  width: 100%;
+  padding: 20px;
+}
+
+.upload-container {
+  display: grid;
+  position: relative;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100%;
+}
+
+.upload-box {
+  position: absolute;
+  top: calc(100vw / 10);
+  left: calc(100vw / 9);
+  width: 100%;
+  max-width: 354px;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+}
+
+main {
+  height: 100%;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  display: inline-block;
+  height: 20px;
+
+  img {
+    height: 100%;
+  }
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.upload-icon {
+  display: block;
+  font-size: 40px;
 }
 </style>
